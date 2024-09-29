d2909f2c3085c503b926d991dafe676d1d1913b3
gjcfe <1627864274@qq.com>
2024-09-29 21:33:43 +0800

    实现stop的a++的bug修复，优化stop功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,6 @@
 import { extend } from "../shared";
-
+let activeEffect;
+let shouldTrack;
 class ReactiveEffect {
   private _fn: any;
   deps = [];
@@ -9,8 +10,18 @@ class ReactiveEffect {
     this._fn = fn;
   }
   run() {
+    // 表示是stop后的状态
+    if (!this.active) {
+      return this._fn();
+    }
+    // 只有set的时候收集依赖
+    shouldTrack = true;
+    //  表示收集依赖
     activeEffect = this;
-    return this._fn();
+    const result = this._fn();
+    // reset
+    shouldTrack = false;
+    return result;
   }
   stop() {
     // 避免stop频繁被调用
@@ -28,10 +39,14 @@ function cleanupEffect(effect) {
   effect.deps.forEach((dep: any) => {
     dep.delete(effect);
   });
+  // 优化点：发现effect.deps为空了
+  effect.deps.length = 0;
 }
 
 const targetMap = new Map();
 export function track(target, key) {
+  if (!isTracking()) return;
+  // target -> key -> dep
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
@@ -42,14 +57,24 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
-  // 只是Reactive的get触发的track的activeEffect为null
-  // 只有effect触发才有值所以if (!activeEffect) return;
-  if (!activeEffect) return;
+  trackEffects(dep);
+}
+export function trackEffects(dep) {
+  // 不要重复添加deep
+  if (dep.has(activeEffect)) return;
+  // 使用全局变量activeEffect将_effect加入dep里面
   dep.add(activeEffect);
-  // stop反向收集依赖
+  // 存储所有的dep，方便stop的时候删除所有的
   activeEffect.deps.push(dep);
 }
-
+export function isTracking() {
+  // 只是Reactive的get触发的track的activeEffect为null
+  // 只有effect触发才有值所以if (!activeEffect) return;
+  // 是否应该收集依赖针对，stop，a++的情况
+  // 针对stop a++ 的情况是否收集依赖
+  // 使用变量 shouldTrack
+  return shouldTrack && activeEffect !== undefined;
+}
 export function trigger(target, key) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
@@ -62,7 +87,6 @@ export function trigger(target, key) {
   }
 }
 
-let activeEffect;
 export function effect(fn, options: any = {}) {
   // fn
   const _effect: any = new ReactiveEffect(fn, options.scheduler);
