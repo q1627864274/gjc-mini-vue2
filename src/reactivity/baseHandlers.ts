573b5bac6838cedb5bbb98fad72c079c599468c7
gjcfe <1627864274@qq.com>
2024-09-29 16:27:34 +0800

    重构reactive中的reactive和readonly函数

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,34 @@
+import { track, trigger } from "./effect";
+
+// 优化点：初始化就调用，防止重复调用
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    if (!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  };
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    trigger(target, key);
+    return res;
+  };
+}
+export const mutableHandlers = {
+  get,
+  set,
+};
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(`key:${key} set 失败因为 target 是 readonly`, target);
+    return true;
+  },
+};
--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -8,4 +8,11 @@ describe("readonly", () => {
     expect(wrapped).not.toBe(original);
     expect(wrapped.foo).toBe(1);
   });
+  it("warn then call set", () => {
+    // mock console.warn
+    console.warn = jest.fn();
+    const user = readonly({ age: 10 });
+    user.age = 11;
+    expect(console.warn).toBeCalled();
+  });
 });
